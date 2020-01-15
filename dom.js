1e550c56322b18eedf8a3255cfde6c8a9885fda2
LogicX1 <uninajone@gmail.com>
2020-01-16 00:08:35 +0200

    organzied code, deleted comments, indecations

--- dom.js
+++ dom.js
@@ -1,187 +1,154 @@
-// part 2 linking it all together
-// The function here is called an iife,
-// it keeps everything inside hidden from the rest of our application
+const submitForm = (function() {
+  var container = document.querySelector(".content");
+  var addTodoForm = document.querySelector(".add-todo");
+  var header = document.querySelector(".header");
+  var removeDone = document.getElementById("removeDone");
 
-const submitForm =
-    (function() {
-        // This is the dom node where we will keep our todo
-        var container = document.querySelector('.content');
-        var addTodoForm = document.querySelector('.add-todo');
-        var header = document.querySelector('.header')
-        var removeDone = document.getElementById('removeDone')
+  var state = [
+    {
+      id: -4,
+      description: '"Refresh" button removes all done tasks',
+      done: false
+    },
+    {
+      id: -3,
+      description: '"checkmark" button sorts by done',
+      done: false
+    },
+    {
+      id: -5,
+      description: '"AZ" button sorts Alphabetically',
+      done: false
+    },
+    {
+      id: -2,
+      description: "To create a new todo use the form bellow",
+      done: false
+    },
+    {
+      id: -1,
+      description: "Be Awesome!",
+      done: true
+    },
+    {
+      id: -0,
+      description: "Love this To-Do list!!",
+      done: true
+    }
+  ];
 
-        var state = [
+  var createTodoNode = function(todo) {
+    var todoNode = document.createElement("li");
+    todoNode.className += " item";
 
-            {
-                id: -4,
-                description: '"Refresh" button removes all done tasks',
-                done: false
-            },
-            {
-                id: -3,
-                description: '"checkmark" button sorts by done',
-                done: false
-            },
-            {
-                id: -5,
-                description: '"AZ" button sorts Alphabetically',
-                done: false
-            },
-            {
-                id: -2,
-                description: 'To create a new todo use the form bellow',
-                done: false
-            },
-            {
-                id: -1,
-                description: 'Be Awesome!',
-                done: true
-            },
-            {
-                id: -0,
-                description: 'Love this To-Do list!!',
-                done: true
-            },
-        ]; // this is our initial todoList
+    // Creating the delete button
+    var deleteButtonNode = document.createElement("i");
+    deleteButtonNode.className += " fas fa-trash";
+    deleteButtonNode.addEventListener("click", function(event) {
+      var newState = todoFunctions.deleteTodo(state, todo.id);
+      update(newState);
+    });
+    todoNode.appendChild(deleteButtonNode);
 
-        // This function takes a todo, it returns the DOM node representing that todo
-        var createTodoNode = function(todo) {
-            var todoNode = document.createElement('li');
-            // you will need to use addEventListener
-            todoNode.className += ' item';
+    // Creating the mark button
+    var markButtonNode = document.createElement("i");
+    markButtonNode.className += " far fa-circle";
 
+    markButtonNode.addEventListener("click", function(event) {
+      var newState = todoFunctions.markTodo(state, todo.id);
+      update(newState);
+    });
+    todoNode.appendChild(markButtonNode);
+    // Creating a span holding description
+    var spanNode = document.createElement("span");
+    var textNode = document.createTextNode(todo.description);
+    spanNode.appendChild(textNode);
+    todoNode.appendChild(spanNode);
+    // Striking the text if the done=true
+    if (todo.done) {
+      spanNode.style.textDecoration = "line-through";
+      markButtonNode.className = " fas fa-circle";
+    }
 
+    // add classes for css
+    spanNode.className += " description";
 
+    return todoNode;
+  };
 
-            // this adds the delete button
-            var deleteButtonNode = document.createElement('i');
-            deleteButtonNode.className += ' fas fa-trash';
-            // deleteButtonNode[aria-hidden]='true';
-            deleteButtonNode.addEventListener('click', function(event) {
-                var newState = todoFunctions.deleteTodo(state, todo.id);
-                update(newState);
-            });
-            todoNode.appendChild(deleteButtonNode);
+  // bind create todo form
+  if (addTodoForm) {
+    addTodoForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      var description = event.target.description.value; // event.target ....
+      var newTodo = todoFunctions.makeNewTodo(
+        undefined,
+        description,
+        undefined
+      );
 
+      createTodoNode(newTodo);
+      var newState = todoFunctions.addTodo(state, newTodo);
+      addTodoForm.reset();
+      update(newState);
+    });
+  }
 
-            // add markTodo button
-            var markButtonNode = document.createElement('i');
-            markButtonNode.className += ' far fa-circle';
-            if (todo.done) {
-                todoNode.style.textDecoration = "line-through"
-            }
-            markButtonNode.addEventListener('click', function(event) {
-                var newState = todoFunctions.markTodo(state, todo.id);
-                update(newState);
-            });
-            todoNode.appendChild(markButtonNode);
-            // add span holding description
-            var spanNode = document.createElement('span');
-            var textNode = document.createTextNode(todo.description);
-            spanNode.appendChild(textNode);
-            todoNode.appendChild(spanNode);
+  // add sort by done button
+  var sortByDoneButton = document.createElement("i");
+  sortByDoneButton.className += " fas fa-check-square";
+  sortByDoneButton.addEventListener("click", function(event) {
+    var newState = todoFunctions.sortTodos(state, (a, b) =>
+      !a.done ? (!b.done ? 0 : -1) : b.done ? 0 : 1
+    );
+    update(newState);
+  });
+  header.appendChild(sortByDoneButton);
 
-            // var markButtonNode = document.createElement('button');
-            // markButtonNode.textContent='Mark'
-            // markButtonNode.addEventListener('click', function(event) {
-            //   var newState = todoFunctions.markTodo(state, todo.id);
-            //   update(newState);
-            // });
-            // todoNode.appendChild(markButtonNode);
+  //Remove marked tasks by clicking reset button
+  removeDone.addEventListener("click", function(event) {
+    var newState = state.filter(e => !e.done);
+    update(newState);
+  });
 
+  // add sort alphabetically
+  var sortByDescriptionButtonNode = document.createElement("i");
+  sortByDescriptionButtonNode.className += " fas fa-sort-alpha-up";
+  sortByDescriptionButtonNode.addEventListener("click", function(event) {
+    var newState = todoFunctions.sortTodos(state, (a, b) =>
+      a.description.toUpperCase().localeCompare(b.description.toUpperCase())
+    );
+    update(newState);
+  });
+  header.appendChild(sortByDescriptionButtonNode);
+  // Bellow are functions that should not to be changed
+  var update = function(newState) {
+    state = newState;
+    renderState(state);
+  };
 
-            // add classes for css
-            spanNode.className += ' description';
+  // you do not need to change this function
+  var renderState = function(state) {
+    var todoListNode = document.createElement("ul");
 
-            return todoNode;
-        };
+    state.forEach(function(todo) {
+      todoListNode.appendChild(createTodoNode(todo));
+    });
 
-        // bind create todo form
-        if (addTodoForm) {
-            addTodoForm.addEventListener('submit', function(event) {
-                // https://developer.mozilla.org/en-US/docs/Web/Events/submit
-                // what does event.preventDefault do?
-                // what is inside event.target?
-                event.preventDefault();
-                var description = event.target.description.value; // event.target ....
-                var newTodo = todoFunctions.makeNewTodo(undefined, description, undefined)
-                    // hint: todoFunctions.addTodo
-                createTodoNode(newTodo);
-                var newState = todoFunctions.addTodo(state, newTodo);
-                addTodoForm.reset();
-                update(newState);
-            });
-        }
+    // you may want to add a class for css
+    container.replaceChild(todoListNode, container.firstChild);
+  };
 
-        // add sort by done button
-        var sortByDoneButton = document.createElement('i');
-        sortByDoneButton.className += ' fas fa-check-square'
-        sortByDoneButton.addEventListener('click', function(event) {
-            var newState = todoFunctions.sortTodos(state, (a, b) => !a.done ? (!b.done ? 0 : -1) : (b.done ? 0 : 1))
-            update(newState);
-        });
-        header.appendChild(sortByDoneButton);
-
-        //Remove done by clicking reset button
-        removeDone.addEventListener('click', function(event) {
-            var newState = state.filter(e => !e.done)
-            update(newState);
-        });
-
-        // add sort alphabetically
-        var sortByDescriptionButtonNode = document.createElement('i');
-        sortByDescriptionButtonNode.className += ' fas fa-sort-alpha-up'
-        sortByDescriptionButtonNode.addEventListener('click', function(event) {
-            var newState = todoFunctions.sortTodos(state, (a, b) => a.description.toUpperCase().localeCompare(b.description.toUpperCase()));
-            update(newState);
-        });
-        header.appendChild(sortByDescriptionButtonNode);
-        // you should not need to change this function
-        var update = function(newState) {
-            state = newState;
-            renderState(state);
-        };
-
-
-        // you do not need to change this function
-        var renderState = function(state) {
-            var todoListNode = document.createElement('ul');
-
-            state.forEach(function(todo) {
-                todoListNode.appendChild(createTodoNode(todo));
-
-
-            });
-
-            // you may want to add a class for css
-            container.replaceChild(todoListNode, container.firstChild);
-        };
-
-        if (container) renderState(state);
-    })();
+  if (container) renderState(state);
+})();
 
 //check for empty text
 function checkForBlank() {
-    if (document.getElementById('txt').value == "") {
-        alert('Please Enter something to remember..')
-        return false;
-    }
+  if (document.getElementById("txt").value == "") {
+    alert("Please Enter something to remember..");
+    return false;
+  }
 }
-
-
-
-//add an item to the list using enter key
-// document.addEventListener('keyup', function(event) {
-//     if (event.keyCode == 13) {
-//         const todo = input.value;
-
-//         if (todo) {
-//             addTodo(todo);
-//         }
-//         input.value = '';
-//     }
-// })
-
-if (typeof module !== 'undefined') {
-    module.exports = submitForm;
+if (typeof module !== "undefined") {
+  module.exports = submitForm;
 }
--- test.js
+++ test.js
@@ -1,115 +1,97 @@
-var todoFunctions = require('./logic');
-var dom = require('./dom');
-const requiredKeys = ['id','description','done'];
+var todoFunctions = require("./logic");
+var dom = require("./dom");
+// Constants for testing purposes
+const requiredKeys = ["id", "description", "done"];
 
 const makeTea = {
-  id: 2, // this is a unique number, it will be needed to find a to-do in a to-do list
-  description: "make tea", // this is a string that describes what you need to do
-  done: false, // This is true or false, it tells us whether a todo is done or not
-}
+  id: 2,
+  description: "make tea",
+  done: false
+};
 
-const toDoArray = [{
-  id:0,
-  description:"Make breakfast",
-  done:false
-},
-{
-  id:1,
-  description:"Do the dishes",
-  done:true
-}]
+const toDoArray = [
+  {
+    id: 0,
+    description: "Make breakfast",
+    done: false
+  },
+  {
+    id: 1,
+    description: "Do the dishes",
+    done: true
+  }
+];
 
-describe('Example test', function() {
-  it('should do something', function() {
+describe("Example test", function() {
+  it("should do something", function() {
     expect(1).toBe(1);
   });
 });
-
-describe('addTodo tests ', function() {
-
-  it('should have a new copy of new to-do at the end', function() {
-    const result = todoFunctions.addTodo(toDoArray,makeTea);
+/*************************   addTodo tests        ***********************/
+describe("addTodo tests ", function() {
+  it("should have a new copy of new to-do at the end", function() {
+    const result = todoFunctions.addTodo(toDoArray, makeTea);
     const actual = makeTea;
     expect(result[result.length - 1]).toEqual(actual);
   });
-  it('should not change the original input toDoArray', function() {
-    const result = todoFunctions.addTodo(toDoArray,makeTea);
+  it("should not change the original input toDoArray", function() {
+    const result = todoFunctions.addTodo(toDoArray, makeTea);
     const actual = toDoArray;
     expect(result).not.toBe(actual);
   });
-  it('should not change the original new todo', function() {
-    const result = todoFunctions.addTodo(toDoArray,makeTea)[toDoArray.length];
+  it("should not change the original new todo", function() {
+    const result = todoFunctions.addTodo(toDoArray, makeTea)[toDoArray.length];
     const actual = makeTea;
     expect(makeTea).toBe(actual);
   });
-  it('should have id,description,done keys', function() {
-    const result = todoFunctions.addTodo(toDoArray,{
-      description:'An object with description only'
+  it("should have id,description,done keys", function() {
+    const result = todoFunctions.addTodo(toDoArray, {
+      description: "An object with description only"
     })[toDoArray.length];
     const resultKeys = Object.keys(result);
     const actual = requiredKeys;
     expect(resultKeys).toEqual(actual);
   });
-
-
 });
 
-
-/*************************   DELETE TO DO        ***********************/
-describe('deleteTodo test', function() {
-
-  it('should be able to delete the first item', function() {
-  const result = todoFunctions.deleteTodo(toDoArray,0);
-  const actual = toDoArray[0]
-  expect(result).not.toContain(actual);
-  });
-  
-  it('should not change the original input todos', function() {
-  const result = todoFunctions.deleteTodo(toDoArray,0)
-  const actual = toDoArray
-  expect(result).not.toBe(actual);
-  });
-  
+/*************************   deleteToDo tests        ***********************/
+describe("deleteTodo test", function() {
+  it("should be able to delete the first item", function() {
+    const result = todoFunctions.deleteTodo(toDoArray, 0);
+    const actual = toDoArray[0];
+    expect(result).not.toContain(actual);
   });
 
-///***********            MARK TO DO        ****************/
-describe('markTodo tests ', function() {
+  it("should not change the original input todos", function() {
+    const result = todoFunctions.deleteTodo(toDoArray, 0);
+    const actual = toDoArray;
+    expect(result).not.toBe(actual);
+  });
+});
 
-  it('should return a new array', function() {
-    const result = todoFunctions.markTodo(toDoArray,0);
+///***********************        markTodo test       ********************/
+describe("markTodo tests ", function() {
+  it("should return a new array", function() {
+    const result = todoFunctions.markTodo(toDoArray, 0);
     const actual = toDoArray;
     expect(result).not.toBe(actual);
   });
 
-  it('should not modify done on the original todos', function() {
+  it("should not modify done on the original todos", function() {
     const actual = toDoArray[0].done;
-    const result = todoFunctions.markTodo(toDoArray,0)[0].done;
+    const result = todoFunctions.markTodo(toDoArray, 0)[0].done;
     expect(result).not.toBe(actual);
   });
 
-  it('should have toggled done for the target todo', function() {
-    const result = todoFunctions.markTodo(toDoArray,0)[0].done;
+  it("should have toggled done for the target todo", function() {
+    const result = todoFunctions.markTodo(toDoArray, 0)[0].done;
     const actual = !toDoArray[0].done;
     expect(result).toBe(actual);
   });
 
-  it('should not toggle other todos', function() {
-    const result = todoFunctions.markTodo(toDoArray,1)[0].done;
+  it("should not toggle other todos", function() {
+    const result = todoFunctions.markTodo(toDoArray, 1)[0].done;
     const actual = toDoArray[0].done;
     expect(result).toBe(actual);
   });
-
 });
-
-
-/*******   create Todo node       **********/
-
-// describe('create TodoNode tests', function() {
-
-//   it('should return a new Todo node in the dom', function() {
-//     const result = dom.createTodoNode(makeTea);
-//     const actual = toDoArray[0].done;
-//     expect(result).toBe(actual);
-//   });
-
-// });
